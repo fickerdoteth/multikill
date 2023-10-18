4b3d3b3113e8cb40b564757b0da133736c0d5ffa
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-18 03:00:58 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,137 +1,80 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-// Character properties
-const character = {
-    x: 50,
-    y: 0,
-    width: 30,
-    height: 30,
-    speed: 3,
-    jumping: false,
-    jumpHeight: 100,
-    jumpCount: 0,
+// Player character
+const player = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    radius: 5,
+    speed: 2,
 };
 
-// Obstacle properties
-const obstacles = [];
-const obstacleWidth = 20;
-const obstacleHeight = 20;
-const obstacleSpeed = 2;
+// Create an array to hold other dots
+const dots = [];
 
 // Game variables
-let score = 0;
 let isGameOver = false;
 
-// Character jumping function
-function jump() {
-    if (!character.jumping) {
-        character.jumping = true;
-        character.jumpCount = 0;
-    }
-}
-
-// Define a retry function to reset the game
-function retry() {
-    isGameOver = false;
-    retryContainer.classList.add('hidden');
-    // Reset game variables or start a new round here...
-    character.x = 50;
-    character.y = 0;
-    obstacles.length = 0; // Clear obstacles
-    score = 0;
-}
-// Retry button event
-function handleRetry() {
-    retry();
-}
-
-retryButton.addEventListener('click', handleRetry);
-
-// Listen for key presses on "Enter" and "Space" buttons
-document.addEventListener('keydown', function (e) {
-    if (e.key === ' ' || e.key === 'Enter') {
-        handleRetry();
-    }
-});
-
-// Game over function
-function gameOver() {
-    isGameOver = true;
-    ctx.fillStyle = '#000';
-    ctx.font = '30px Arial';
-    ctx.fillText('Game Over', canvas.width / 2 - 70, canvas.height / 2);
+// Function to check collision between two dots
+function isCollision(dot1, dot2) {
+    const dx = dot1.x - dot2.x;
+    const dy = dot1.y - dot2.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance < dot1.radius + dot2.radius;
 }
 
 // Game loop
 function gameLoop() {
     if (isGameOver) return;
 
-
-    
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw character
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, canvas.height - character.height - character.y, character.width, character.height);
-
-    // Draw character in white
+    // Draw player
     ctx.fillStyle = '#fff';
-    ctx.fillRect(character.x, canvas.height - character.height - character.y, character.width, character.height);
+    ctx.beginPath();
+    ctx.arc(player.x, player.y, player.radius, 0, Math.PI * 2);
+    ctx.fill();
 
-    // Draw obstacles
-    for (let i = 0; i < obstacles.length; i++) {
-        const obstacle = obstacles[i];
-        ctx.fillStyle = '#333';
-        ctx.fillRect(obstacle.x, canvas.height - obstacle.height, obstacleWidth, obstacleHeight);
+    // Draw other dots, check for collisions
+    for (let i = 0; i < dots.length; i++) {
+        const dot = dots[i];
+        ctx.fillStyle = '#fff';
+        ctx.beginPath();
+        ctx.arc(dot.x, dot.y, dot.radius, 0, Math.PI * 2);
+        ctx.fill();
 
-        // Check collision
-        if (character.x < obstacle.x + obstacleWidth &&
-            character.x + character.width > obstacle.x &&
-            character.y < obstacle.height) {
-            gameOver();
-        }
-
-        obstacle.x -= obstacleSpeed;
-
-        if (obstacle.x + obstacleWidth < 0) {
-            obstacles.splice(i, 1);
-            i--;
-            score++;
+        if (isCollision(player, dot)) {
+            if (player.radius > dot.radius) {
+                // Player eats the dot
+                dots.splice(i, 1);
+                player.radius += 1; // Player gets bigger
+            } else {
+                // Player is eaten by the dot
+                isGameOver = true;
+            }
         }
     }
 
-    // Update character position
-    if (character.jumping) {
-        character.y += character.speed * 2;
-        character.jumpCount += character.speed * 2;
-        if (character.jumpCount >= character.jumpHeight) {
-            character.jumping = false;
-            character.jumpCount = 0;
-        }
-    } else if (character.y > 0) {
-        character.y -= character.speed;
-    }
+    // Update player position based on key input or mouse input
+    // (You can add input handling for player movement here)
 
-    // Draw score
-    ctx.fillStyle = '#000';
-    ctx.font = '20px Arial';
-    ctx.fillText('Score: ' + score, 20, 20);
-
-    // Generate new obstacles
+    // Generate new dots
     if (Math.random() < 0.02) {
-        obstacles.push({ x: canvas.width, height: Math.random() * 100 + 20 });
+        dots.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            radius: Math.random() * 5 + 2,
+        });
     }
 
     requestAnimationFrame(gameLoop);
 }
 
-// Handle key press for jumping
-document.addEventListener('keydown', function (e) {
-    if (e.key === ' ' || e.key === 'ArrowUp') {
-        jump();
-    }
+// Handle key/mouse input for player movement
+document.addEventListener('mousemove', (e) => {
+    player.x = e.clientX - canvas.getBoundingClientRect().left;
+    player.y = e.clientY - canvas.getBoundingClientRect().top;
 });
 
 gameLoop();
