96e8ae29b1327079edea22541ff3d0642486a1d9
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-22 03:51:43 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,68 +1,97 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-const playerFish = {
+const player = {
     x: canvas.width / 2,
     y: canvas.height / 2,
-    radius: 15,
-    color: 'blue',
-    speed: 5,
+    size: 30,
+    dx: 0,
+    dy: 0,
+    speed: 4
 };
 
-const smallerFishes = [];
-const numSmallerFishes = 50;
+const fishArray = [];
 
-for (let i = 0; i < numSmallerFishes; i++) {
-    smallerFishes.push({
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        radius: 5,
-        color: 'green',
-    });
-}
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = playerFish.color;
+function drawPlayer() {
+    ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(playerFish.x, playerFish.y, playerFish.radius, 0, Math.PI * 2);
+    ctx.arc(player.x, player.y, player.size, 0, Math.PI * 2);
     ctx.fill();
-
-    document.addEventListener('keydown', (e) => {
-        switch (e.key) {
-            case 'ArrowUp':
-                playerFish.y -= playerFish.speed;
-                break;
-            case 'ArrowDown':
-                playerFish.y += playerFish.speed;
-                break;
-            case 'ArrowLeft':
-                playerFish.x -= playerFish.speed;
-                break;
-            case 'ArrowRight':
-                playerFish.x += playerFish.speed;
-                break;
-        }
-    });
-
-    smallerFishes.forEach((fish) => {
-        ctx.fillStyle = fish.color;
-        ctx.beginPath();
-        ctx.arc(fish.x, fish.y, fish.radius, 0, Math.PI * 2);
-        ctx.fill();
-
-        const dx = playerFish.x - fish.x;
-        const dy = playerFish.y - fish.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-
-        if (distance < playerFish.radius + fish.radius) {
-            smallerFishes.splice(smallerFishes.indexOf(fish), 1);
-            playerFish.radius += 1;
-        }
-    });
-
-    requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+function updatePlayer() {
+    player.x += player.dx;
+    player.y += player.dy;
+}
+
+function movePlayer(e) {
+    switch(e.keyCode) {
+        case 37: // Left
+            player.dx = -player.speed;
+            player.dy = 0;
+            break;
+        case 38: // Up
+            player.dy = -player.speed;
+            player.dx = 0;
+            break;
+        case 39: // Right
+            player.dx = player.speed;
+            player.dy = 0;
+            break;
+        case 40: // Down
+            player.dy = player.speed;
+            player.dx = 0;
+            break;
+    }
+}
+
+function spawnFish() {
+    const size = Math.random() * (player.size - 10) + 10;
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+
+    fishArray.push({
+        x,
+        y,
+        size
+    });
+}
+
+function drawFish() {
+    ctx.fillStyle = "blue";
+    for(let fish of fishArray) {
+        ctx.beginPath();
+        ctx.arc(fish.x, fish.y, fish.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function eatFish() {
+    for(let i = 0; i < fishArray.length; i++) {
+        let fish = fishArray[i];
+        let distance = Math.sqrt((player.x - fish.x) ** 2 + (player.y - fish.y) ** 2);
+
+        if(distance < player.size + fish.size) {
+            fishArray.splice(i, 1);
+            player.size += 1;
+            spawnFish();
+        }
+    }
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPlayer();
+    drawFish();
+    updatePlayer();
+    eatFish();
+    requestAnimationFrame(update);
+}
+
+document.addEventListener("keydown", movePlayer);
+
+for(let i = 0; i < 20; i++) {
+    spawnFish();
+}
+
+update();
