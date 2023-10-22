54d53c993304e6e0b12040ec4b36446ec69e303c
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-22 03:09:47 +0200

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const player = {
     x: canvas.width / 2,
     y: canvas.height / 2,
     size: 20,
-    speed: 2,
+    speed: 4, // Adjusted player speed
 };
 
 // Blocks on the screen
@@ -44,18 +44,26 @@ function gameLoop() {
 
         if (isCollision(player, block)) {
             if (player.size > block.size) {
-                // Player grows by a little
-                player.size += 2;
+                // Player grows by a little (reduced growth rate)
+                player.size += 1;
                 blocks.splice(i, 1);
                 i--;
             } else {
                 isGameOver = true;
             }
         }
+
+        // Make enemy blocks move slowly (update block positions)
+        block.x += (Math.random() - 0.5); // Small random horizontal movement
+        block.y += (Math.random() - 0.5); // Small random vertical movement
     }
 
-    // Update player position based on key input
-    // (You can add input handling for player movement here)
+    // Update player position based on key input (adjusted player speed)
+    // WASD controls
+    if (keys.w) player.y -= player.speed;
+    if (keys.s) player.y += player.speed;
+    if (keys.a) player.x -= player.speed;
+    if (keys.d) player.x += player.speed;
 
     // Generate new blocks
     if (Math.random() < 0.02) {
@@ -69,22 +77,56 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
 }
 
-// Handle key input for player movement (arrow keys)
+// Handle key input for player movement (WASD controls)
+const keys = {
+    w: false,
+    a: false,
+    s: false,
+    d: false,
+};
+
 document.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case 'ArrowUp':
-            player.y -= player.speed;
-            break;
-        case 'ArrowDown':
-            player.y += player.speed;
-            break;
-        case 'ArrowLeft':
-            player.x -= player.speed;
-            break;
-        case 'ArrowRight':
-            player.x += player.speed;
-            break;
-    }
+    if (e.key === 'w') keys.w = true;
+    if (e.key === 'a') keys.a = true;
+    if (e.key === 's') keys.s = true;
+    if (e.key === 'd') keys.d = true;
+});
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'w') keys.w = false;
+    if (e.key === 'a') keys.a = false;
+    if (e.key === 's') keys.s = false;
+    if (e.key === 'd') keys.d = false;
 });
 
 gameLoop();
+
+// Retry button event
+const retryButton = document.createElement('button');
+retryButton.textContent = 'Retry';
+retryButton.style.position = 'absolute';
+retryButton.style.left = '50%';
+retryButton.style.top = '50%';
+retryButton.style.transform = 'translate(-50%, -50%)';
+retryButton.style.display = 'none';
+
+document.body.appendChild(retryButton);
+
+// Function to handle retry
+function retryGame() {
+    isGameOver = false;
+    player.size = 20; // Reset player size
+    player.x = canvas.width / 2;
+    player.y = canvas.height / 2;
+    blocks.length = 0; // Clear blocks
+    retryButton.style.display = 'none';
+}
+
+retryButton.addEventListener('click', retryGame);
+
+// Retry button can be pressed with "space" or "enter" key
+document.addEventListener('keydown', (e) => {
+    if ((e.key === ' ' || e.key === 'Enter') && isGameOver) {
+        retryGame();
+    }
+});
