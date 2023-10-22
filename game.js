62fe6001d5b52f715011e19e9f52058cd0c100ab
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-22 03:17:10 +0200

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const player = {
     x: canvas.width / 2,
     y: canvas.height / 2,
     size: 20,
-    speed: 4, // Adjusted player speed
+    speed: 10,
 };
 
 // Blocks on the screen
@@ -23,6 +23,17 @@ function isCollision(block1, block2) {
         block1.y < block2.y + block2.size &&
         block1.y + block1.size > block2.y
     );
+}
+
+// Function to give blocks random movements
+function moveBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = blocks[i];
+
+        // Random movement
+        block.x += (Math.random() - 0.5) * 5; // Adjust the 5 to control block movement speed
+        block.y += (Math.random() - 0.5) * 5; // Adjust the 5 to control block movement speed
+    }
 }
 
 // Game loop
@@ -44,26 +55,21 @@ function gameLoop() {
 
         if (isCollision(player, block)) {
             if (player.size > block.size) {
-                // Player grows by a little (reduced growth rate)
-                player.size += 1;
+                // Player grows by a little
+                player.size += 2;
                 blocks.splice(i, 1);
                 i--;
             } else {
                 isGameOver = true;
             }
         }
-
-        // Make enemy blocks move slowly (update block positions)
-        block.x += (Math.random() - 0.5); // Small random horizontal movement
-        block.y += (Math.random() - 0.5); // Small random vertical movement
     }
 
-    // Update player position based on key input (adjusted player speed)
-    // WASD controls
-    if (keys.w) player.y -= player.speed;
-    if (keys.s) player.y += player.speed;
-    if (keys.a) player.x -= player.speed;
-    if (keys.d) player.x += player.speed;
+    // Update player position based on key input
+    // (You can add input handling for player movement here)
+
+    // Give blocks random movements
+    moveBlocks();
 
     // Generate new blocks
     if (Math.random() < 0.02) {
@@ -77,56 +83,22 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
 }
 
-// Handle key input for player movement (WASD controls)
-const keys = {
-    w: false,
-    a: false,
-    s: false,
-    d: false,
-};
-
+// Handle key input for player movement (arrow keys)
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'w') keys.w = true;
-    if (e.key === 'a') keys.a = true;
-    if (e.key === 's') keys.s = true;
-    if (e.key === 'd') keys.d = true;
-});
-
-document.addEventListener('keyup', (e) => {
-    if (e.key === 'w') keys.w = false;
-    if (e.key === 'a') keys.a = false;
-    if (e.key === 's') keys.s = false;
-    if (e.key === 'd') keys.d = false;
+    switch (e.key) {
+        case 'ArrowUp':
+            player.y -= player.speed;
+            break;
+        case 'ArrowDown':
+            player.y += player.speed;
+            break;
+        case 'ArrowLeft':
+            player.x -= player.speed;
+            break;
+        case 'ArrowRight':
+            player.x += player.speed;
+            break;
+    }
 });
 
 gameLoop();
-
-// Retry button event
-const retryButton = document.createElement('button');
-retryButton.textContent = 'Retry';
-retryButton.style.position = 'absolute';
-retryButton.style.left = '50%';
-retryButton.style.top = '50%';
-retryButton.style.transform = 'translate(-50%, -50%)';
-retryButton.style.display = 'none';
-
-document.body.appendChild(retryButton);
-
-// Function to handle retry
-function retryGame() {
-    isGameOver = false;
-    player.size = 20; // Reset player size
-    player.x = canvas.width / 2;
-    player.y = canvas.height / 2;
-    blocks.length = 0; // Clear blocks
-    retryButton.style.display = 'none';
-}
-
-retryButton.addEventListener('click', retryGame);
-
-// Retry button can be pressed with "space" or "enter" key
-document.addEventListener('keydown', (e) => {
-    if ((e.key === ' ' || e.key === 'Enter') && isGameOver) {
-        retryGame();
-    }
-});
