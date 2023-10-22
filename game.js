9ba123e7a071a3eb35c4cd69f29289904977c5d0
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-22 03:00:01 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,26 +1,28 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-// Player character
+// Player block
 const player = {
     x: canvas.width / 2,
     y: canvas.height / 2,
-    radius: 5,
+    size: 20,
     speed: 2,
 };
 
-// Create an array to hold other dots (enemies)
-const enemies = [];
+// Blocks on the screen
+const blocks = [];
 
 // Game variables
 let isGameOver = false;
 
-// Function to check collision between two dots
-function isCollision(dot1, dot2) {
-    const dx = dot1.x - dot2.x;
-    const dy = dot1.y - dot2.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    return distance < dot1.radius + dot2.radius;
+// Function to check collision between two blocks
+function isCollision(block1, block2) {
+    return (
+        block1.x < block2.x + block2.size &&
+        block1.x + block1.size > block2.x &&
+        block1.y < block2.y + block2.size &&
+        block1.y + block1.size > block2.y
+    );
 }
 
 // Game loop
@@ -30,45 +32,59 @@ function gameLoop() {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw player
-    ctx.fillStyle = '#fff';
-    ctx.beginPath();
-    ctx.arc(player.x, player.y, player.radius, 0, Math.PI * 2);
-    ctx.fill();
+    // Draw player block
+    ctx.fillStyle = '#000';
+    ctx.fillRect(player.x, player.y, player.size, player.size);
 
-    // Draw enemies (other dots) and check for collisions
-    for (let i = 0; i < enemies.length; i++) {
-        const enemy = enemies[i];
-        ctx.fillStyle = '#fff';
-        ctx.beginPath();
-        ctx.arc(enemy.x, enemy.y, enemy.radius, 0, Math.PI * 2);
-        ctx.fill();
+    // Draw and check collisions with other blocks
+    for (let i = 0; i < blocks.length; i++) {
+        const block = blocks[i];
+        ctx.fillStyle = '#000';
+        ctx.fillRect(block.x, block.y, block.size, block.size);
 
-        if (isCollision(player, enemy)) {
-            isGameOver = true; // Game over when player touches an enemy
+        if (isCollision(player, block)) {
+            if (player.size > block.size) {
+                // Player grows by a little
+                player.size += 2;
+                blocks.splice(i, 1);
+                i--;
+            } else {
+                isGameOver = true;
+            }
         }
     }
 
-    // Update player position based on key input or mouse input
+    // Update player position based on key input
     // (You can add input handling for player movement here)
 
-    // Generate new enemies (other dots)
+    // Generate new blocks
     if (Math.random() < 0.02) {
-        enemies.push({
-            x: Math.random() * canvas.width,
-            y: Math.random() * canvas.height,
-            radius: Math.random() * 10 + 5,
+        blocks.push({
+            x: Math.random() * (canvas.width - player.size),
+            y: Math.random() * (canvas.height - player.size),
+            size: Math.random() * 15 + 10,
         });
     }
 
     requestAnimationFrame(gameLoop);
 }
 
-// Handle mouse input for player movement
-canvas.addEventListener('mousemove', (e) => {
-    const rect = canvas.getBoundingClientRect();
-    player.x = e.clientX - rect.left;
-    player.y = e.clientY - rect.top;
+// Handle key input for player movement (arrow keys)
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'ArrowUp':
+            player.y -= player.speed;
+            break;
+        case 'ArrowDown':
+            player.y += player.speed;
+            break;
+        case 'ArrowLeft':
+            player.x -= player.speed;
+            break;
+        case 'ArrowRight':
+            player.x += player.speed;
+            break;
+    }
 });
 
 gameLoop();
