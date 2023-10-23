5ce7a474284d83ed431f9df9bbb83c806bb956d0
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-23 06:09:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,52 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-// Set canvas dimensions
-canvas.width = 1080;
-canvas.height = 720;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const player = {
   x: canvas.width / 2,
   y: canvas.height / 2,
   radius: 20,
-  speed: 5,
+  speed: 2,
+  velocityX: 0,
+  velocityY: 0,
+  friction: 0.98, // Friction factor to slow down the player
 };
 
-// Dot properties
-const dot = {
-  x: canvas.width / 2,
-  y: canvas.height / 2,
-  size: 5,
-  speedX: 0, // Initial horizontal speed
-  speedY: 0, // Initial vertical speed
-  acceleration: 2.5, // Acceleration factor
-  friction: 0.95, // Friction factor
-};
-
-// Enemy properties
 const enemies = [];
+const maxEnemies = 10;
 
-function createRandomEnemy() {
- 
-  // Create a new enemy from the left or right side with random speed
+function createEnemy() {
   const fromLeft = Math.random() < 0.5;
   const x = fromLeft ? -20 : canvas.width + 20;
   const y = Math.random() * canvas.height;
-  const speedX = fromLeft ? (1 + Math.random() * 2) : (-1 - Math.random() * 2);
-  
-  enemies.push({ x, y, size: 20, speedX });
-}
-const foods = [];
-const maxFoods = 30;
+  const speedX = fromLeft ? 1 + Math.random() * 2 : -1 - Math.random() * 2;
 
-function createFood() {
-  const x = Math.random() * canvas.width;
-  const y = Math.random() * canvas.height;
-  const radius = Math.random() * 10 + 5;
-
-  foods.push({ x, y, radius });
+  enemies.push({ x, y, radius: 20, speedX });
 }
 
-function handleFoods() {
-  if (foods.length < maxFoods) {
-    createFood();
+function handleEnemies() {
+  if (enemies.length < maxEnemies) {
+    createEnemy();
   }
 
-  for (let i = foods.length - 1; i >= 0; i--) {
-    const food = foods[i];
-    const dx = food.x - player.x;
-    const dy = food.y - player.y;
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    const enemy = enemies[i];
+    enemy.x += enemy.speedX;
+
+    if (enemy.x < -20 || enemy.x > canvas.width + 20) {
+      enemies.splice(i, 1);
+      createEnemy();
+    }
+
+    const dx = enemy.x - player.x;
+    const dy = enemy.y - player.y;
     const distance = Math.sqrt(dx * dx + dy * dy);
 
-    if (distance < player.radius + food.radius) {
-      foods.splice(i, 1);
-      player.radius += 2;
+    if (distance < player.radius + enemy.radius) {
+      // Handle collision, for example, end the game
+      // You can add your custom game logic here
     }
   }
 }
@@ -72,22 +58,71 @@ function drawPlayer() {
   ctx.fill();
 }
 
-function drawFood() {
+function drawEnemies() {
   ctx.fillStyle = "#33FF66";
-  for (const food of foods) {
+  for (const enemy of enemies) {
     ctx.beginPath();
-    ctx.arc(food.x, food.y, food.radius, 0, Math.PI * 2);
+    ctx.arc(enemy.x, enemy.y, enemy.radius, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
+function movePlayer() {
+  player.velocityX *= player.friction;
+  player.velocityY *= player.friction;
+
+  if (keys["ArrowUp"]) {
+    player.velocityY -= player.speed;
+  }
+  if (keys["ArrowDown"]) {
+    player.velocityY += player.speed;
+  }
+  if (keys["ArrowLeft"]) {
+    player.velocityX -= player.speed;
+  }
+  if (keys["ArrowRight"]) {
+    player.velocityX += player.speed;
+  }
+
+  player.x += player.velocityX;
+  player.y += player.velocityY;
+
+  // Keep the player within the canvas boundaries
+  if (player.x - player.radius < 0) {
+    player.x = player.radius;
+    player.velocityX = 0;
+  } else if (player.x + player.radius > canvas.width) {
+    player.x = canvas.width - player.radius;
+    player.velocityX = 0;
+  }
+
+  if (player.y - player.radius < 0) {
+    player.y = player.radius;
+    player.velocityY = 0;
+  } else if (player.y + player.radius > canvas.height) {
+    player.y = canvas.height - player.radius;
+    player.velocityY = 0;
+  }
+}
+
+const keys = {};
+
+window.addEventListener("keydown", (e) => {
+  keys[e.key] = true;
+});
+
+window.addEventListener("keyup", (e) => {
+  keys[e.key] = false;
+});
+
 function update() {
-  handleFoods();
+  movePlayer();
+  handleEnemies();
 }
 
 function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawFood();
+  drawEnemies();
   drawPlayer();
 }
 
