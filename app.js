1fb8c50440604150ac325e6ae3240bf04eb0385e
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-23 02:05:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,87 +1,76 @@
-document.addEventListener('DOMContentLoaded' , () => {
-    const bird = document.querySelector('.bird')
-    const gameDisplay = document.querySelector('.game-container')
-    const ground = document.querySelector('.ground-moving')
+import pygame
 
-    let birdLeft = 220
-    let birdBottom = 100
-    let gravity = 3
-    let isGameOver = false
-    let gap = 430
+pygame.init()
 
+# Constants
+WIDTH, HEIGHT = 1080, 720
+PLAYER_RADIUS = 5
+PLAYER_SPEED = 0.2
+ACCELERATION = 0.001  # Acceleration factor
+BACKGROUND_COLOR = (0, 0, 0)  # RGB color
 
-    function startGame() {
-        birdBottom -= gravity
-        bird.style.bottom = birdBottom + 'px'
-        bird.style.left = birdLeft + 'px'
-    }
-    let gameTimerId = setInterval(startGame, 20)
+# Colors
+WHITE = (255, 255, 255)
 
-    function control(e) {
-        if (e.keyCode === 32) {
-            jump()
-        }
-    }
+# Initialize the screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Multikill")
 
-    function jump() {
-        if (birdBottom < 500) birdBottom += 50
-        bird.style.bottom = birdBottom + 'px'
-        console.log(birdBottom)
-    }
-    document.addEventListener('keyup', control)
+# Create the player fish
+player_x = WIDTH // 2
+player_y = HEIGHT // 2
 
+# Initialize velocity for smoother movement
+player_velocity_x = 0.
+player_velocity_y = 0.
 
-    function generateObstacle() {
-        let obstacleLeft = 500
-        let randomHeight = Math.random() * 60
-        let obstacleBottom = randomHeight
-        const obstacle = document.createElement('div')
-        const topObstacle = document.createElement('div')
-        if (!isGameOver) {
-            obstacle.classList.add('obstacle')
-            topObstacle.classList.add('topObstacle')
-        }
-        gameDisplay.appendChild(obstacle)
-        gameDisplay.appendChild(topObstacle)
-        obstacle.style.left = obstacleLeft + 'px'
-        topObstacle.style.left = obstacleLeft + 'px'
-        obstacle.style.bottom = obstacleBottom + 'px'
-        topObstacle.style.bottom = obstacleBottom + gap + 'px'
+# Game loop
+running = True
+font = pygame.font.Font(None, 36)
 
-        function moveObstacle() {
-            obstacleLeft -=2
-            obstacle.style.left = obstacleLeft + 'px'
-            topObstacle.style.left = obstacleLeft + 'px'
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-            if (obstacleLeft === -60) {
-                clearInterval(timerId)
-                gameDisplay.removeChild(obstacle)
-                gameDisplay.removeChild(topObstacle)
-            }
-            if (
-                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -200)||
-                birdBottom === 0 
-                ) {
-                gameOver()
-                clearInterval(timerId)
-            }
-        }
-        let timerId = setInterval(moveObstacle, 20) 
-        if (!isGameOver) setTimeout(generateObstacle, 3000)
+    # Move the player fish with acceleration
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] or keys[pygame.K_w] and player_y > 0.1:
+        player_velocity_y -= ACCELERATION
+    if keys[pygame.K_DOWN] or keys[pygame.K_s] and player_y < HEIGHT:
+        player_velocity_y += ACCELERATION
+    if keys[pygame.K_LEFT] or keys[pygame.K_a] and player_x > 0.1:
+        player_velocity_x -= ACCELERATION
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d] and player_x < WIDTH:
+        player_velocity_x += ACCELERATION
 
-    }
-    generateObstacle()
+    # Apply velocity to the player's position
+    player_x += player_velocity_x
+    player_y += player_velocity_y
 
+    # Prevent the player from leaving the window
+    if player_x < 0:
+        player_x = 0
+        player_velocity_x = 0
+    if player_x > WIDTH:
+        player_x = WIDTH
+        player_velocity_x = 0
+    if player_y < 0:
+        player_y = 0
+        player_velocity_y = 0
+    if player_y > HEIGHT:
+        player_y = HEIGHT
+        player_velocity_y = 0
 
-    function gameOver() {
-        clearInterval(gameTimerId)
-        console.log('game over')
-        isGameOver = true
-        document.removeEventListener('keyup', control)
-        ground.classList.add('ground')
-        ground.classList.remove('ground-moving')
-    }
+    # Slow down the player's velocity
+    player_velocity_x *= .999
+    player_velocity_y *= .999
 
+    # Draw the player fish
+    screen.fill(BACKGROUND_COLOR)
+    pygame.draw.circle(screen, WHITE, (player_x, player_y), PLAYER_RADIUS)
+    pygame.display.update()
 
-})
+# Clean up
+pygame.quit()
+
