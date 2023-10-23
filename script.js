93be6955a5fd355ca04e7a4a8c7bfe9e7d5d1afb
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-24 01:16:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -87,12 +87,13 @@ function handleEnemies() {
         player.score += points; // Update the player's score
         enemies.splice(i, 1); // Remove the smaller enemy
       } else {
-        
         // Player touches a bigger enemy, it's game over
-        alert("Game Over! Score: " + player.score + 
-              
-              " Press OK to try again ");
-        document.location.reload();
+        const confirmation = window.confirm("Game Over! Score: " + player.score + "\nPress OK to try again");
+        if (confirmation) {
+          player.score = 0; // Reset the score
+          player.radius = 3; // Reset the player's size
+          enemies.length = 0; // Clear the enemies
+        }
       }
     }
   }
@@ -115,7 +116,7 @@ function drawEnemies() {
 }
 
 function drawScore() {
-  ctx.fillStyle = "#FF10F0 0.5)"; // Transparent neon pink
+  ctx.fillStyle = "rgba(255, 16, 240, 0.5)"; // Transparent neon pink
   ctx.font = "24px Arial";
   ctx.textAlign = "right";
   ctx.fillText("Score: " + player.score, canvas.width - 20, canvas.height - 20);
@@ -125,16 +126,16 @@ function movePlayer() {
   player.velocityX *= player.friction;
   player.velocityY *= player.friction;
 
-  if (keys["ArrowUp"]) {
+  if (keys["ArrowUp"] || keys["w"]) {
     player.velocityY -= player.speed;
   }
-  if (keys["ArrowDown"]) {
+  if (keys["ArrowDown"] || keys["s"]) {
     player.velocityY += player.speed;
   }
-  if (keys["ArrowLeft"]) {
+  if (keys["ArrowLeft"] || keys["a"]) {
     player.velocityX -= player.speed;
   }
-  if (keys["ArrowRight"]) {
+  if (keys["ArrowRight"] || keys["d"]) {
     player.velocityX += player.speed;
   }
 
@@ -189,3 +190,4 @@ function gameLoop() {
 }
 
 gameLoop();
+
