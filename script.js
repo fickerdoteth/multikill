d8df9afe87534e1153a02febb48d64cb941ec72b
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-27 07:31:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-
-// Audio element.
 const bgMusic = document.getElementById("bgMusic");
 
 canvas.width = 1080;
